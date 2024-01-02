67203ac7f40043fcc1a3fa43531beb5eee855306
NineBerry <nineberry@nineberry.de>
2024-01-02 02:43:41 +0100

    Working more on 2015

---
+++ 2015/Day16/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.RegularExpressions;
+
+{
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2015\Day16\Full.txt";
+
+    Dictionary<string, int> descriptions = new()
+    {
+        { "children", 3 },
+        { "cats", 7 },
+        { "samoyeds", 2 },
+        { "pomeranians", 3 },
+        { "akitas", 0 },
+        { "vizslas", 0 },
+        { "goldfish", 5 },
+        { "trees", 3 },
+        { "cars", 2 },
+        { "perfumes", 1 }
+    };
+
+    string[] input = File.ReadAllLines(fileName);
+    Sue sueDescription = new Sue(-1, descriptions);
+
+    IEnumerable<Sue> sues = input.Select(line => new Sue(line));
+
+    Console.WriteLine("Part 1: " + Part1(sues, sueDescription));
+    Console.WriteLine("Part 2: " + Part2(sues, sueDescription));
+    Console.ReadLine();
+}
+
+
+long Part1(IEnumerable<Sue> sues, Sue sueDescription)
+{
+    return sues
+        .First(sue => sue.FitsDescription(sueDescription, [], []))
+        .Number;
+}
+
+long Part2(IEnumerable<Sue> sues, Sue sueDescription)
+{
+    HashSet<string> underratedProperties = ["cats", "trees"];
+    HashSet<string> overratedProperties = ["pomeranians", "goldfish"];
+
+    return sues
+        .First(sue => sue.FitsDescription(sueDescription, underratedProperties, overratedProperties))
+        .Number;
+}
+
+class Sue 
+{
+    public int Number { get; set; }
+    public Dictionary<string, int> Properties { get; set; }
+
+    public Sue(int number, Dictionary<string, int> properties)
+    {
+        Number = number;
+        Properties = properties;
+    }
+
+    public Sue(string input)
+    {
+        Number = int.Parse(Regex.Match(input, "Sue (\\d+):").Groups[1].Value);
+
+        Properties =
+            Regex.Matches(input, "([a-z]+): (\\d+)")
+            .Select(m => (m.Groups[1].Value, int.Parse(m.Groups[2].Value)))
+            .ToDictionary();
+    }
+
+    public bool FitsDescription(Sue description, 
+        HashSet<string> underratedProperties, 
+        HashSet<string> overratedProperties)
+    {
+        foreach (var attribute in description.Properties)
+        {
+            if(Properties.TryGetValue(attribute.Key, out int value))
+            {
+                if (underratedProperties.Contains(attribute.Key))
+                {
+                    if (attribute.Value >= value) return false;
+                }
+                else if (overratedProperties.Contains(attribute.Key))
+                {
+                    if (attribute.Value <= value) return false;
+                }
+                else
+                {
+                    if (attribute.Value != value) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ 2015/Day17/Program.cs
@@ -0,0 +1,60 @@
+﻿// #define Sample
+{
+
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2015\Day17\Sample.txt";
+    int eggnog = 25;
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2015\Day17\Full.txt";
+    int eggnog = 150;
+#endif
+
+    string[] input = File.ReadAllLines(fileName);
+    int[] containers = input.Select(int.Parse).OrderDescending().ToArray();
+
+    Console.WriteLine("Part 1: " + Part1(containers, eggnog));
+    Console.WriteLine("Part 2: " + Part2(containers, eggnog));
+    Console.ReadLine();
+}
+
+long Part1(int[] containers, int eggnog)
+{
+    var combinations = FillContainers(eggnog, [], containers);
+    return combinations.Count;
+}
+
+long Part2(int[] containers, int eggnog)
+{
+    var combinations = FillContainers(eggnog, [], containers);
+    int minContainers = combinations.Select(c => c.Length).Min();
+    return combinations.Count(c => c.Length == minContainers);
+}
+
+List<int[]> FillContainers(int eggnog, int[] fullContainers, int[] emptyContainers)
+{
+    if(eggnog == 0)
+    {
+        return [fullContainers];
+    }
+
+    if(eggnog < 0)
+    {
+        return [];
+    }
+    
+    if(emptyContainers is [])
+    {
+        return [];
+    }
+
+    List<int[]> result = new();
+
+    for(int i=0; i < emptyContainers.Length; i++)
+    {
+        int nextContainer = emptyContainers[i];
+        var newCombinations = FillContainers(eggnog - nextContainer, [..fullContainers, nextContainer], emptyContainers.Skip(i + 1).ToArray());
+        result.AddRange(newCombinations);
+    }
+
+    return result;
+}

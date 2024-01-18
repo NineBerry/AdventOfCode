5188988e98f253498a986548b22ab9de13135e63
NineBerry <nineberry@nineberry.de>
2024-01-19 00:06:25 +0100

    2017 Day 01 and 02

---
+++ 2017/Day01/Program.cs
@@ -0,0 +1,37 @@
+﻿// #define Sample
+{
+
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2017\Day01\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2017\Day01\Full.txt";
+#endif
+
+    string input = File.ReadAllText(fileName);
+
+    Console.WriteLine("Part 1: " + Part1(input));
+    Console.WriteLine("Part 2: " + Part2(input));
+    Console.ReadLine();
+}
+
+long Part1(string input)
+{
+    return Solve(input, 1);
+}
+
+long Part2(string input)
+{
+    return Solve(input, input.Length / 2);
+}
+
+long Solve(string input, int offset)
+{
+    input += input.Substring(0, offset);
+
+    return
+        input
+        .Zip(input.Skip(offset))
+        .Where(pair => pair.First == pair.Second)
+        .Select(pair => pair.First - '0')
+        .Sum();
+}
---
+++ 2017/Day02/Program.cs
@@ -0,0 +1,48 @@
+﻿// #define Sample
+using System.Text.RegularExpressions;
+
+{
+
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2017\Day02\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2017\Day02\Full.txt";
+#endif
+
+    IEnumerable<int[]> input = File.ReadAllLines(fileName).Select(ParseLine);
+
+    Console.WriteLine("Part 1: " + Part1(input));
+    Console.WriteLine("Part 2: " + Part2(input));
+    Console.ReadLine();
+}
+
+long Part1(IEnumerable<int[]> input)
+{
+    return input.Sum(values => values.Max() - values.Min());
+}
+
+long Part2(IEnumerable<int[]> input)
+{
+    
+    return input.Sum(GetEvenDivision);
+}
+
+int GetEvenDivision(int[] values)
+{
+    for (int i = 0; i < values.Length; i++)
+    {
+        for (int j = 0; j < values.Length; j++)
+        {
+            if (i == j) continue;
+
+            if (values[i] % values[j] == 0) return values[i] / values[j];
+        }
+    }
+
+    return 0;
+}
+
+int[] ParseLine(string line)
+{
+    return Regex.Matches(line, "\\d+").Select(m => int.Parse(m.Value)).ToArray();   
+}

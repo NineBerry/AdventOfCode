58d8ac3161a5a66de927c3ec6ae0be1387047b90
NineBerry <nineberry@nineberry.de>
2024-02-04 22:11:03 +0100

    2020 Day 06

---
+++ 2020/Day06/Program.cs
@@ -0,0 +1,38 @@
+﻿// #define Sample
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2020\Day06\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2020\Day06\Full.txt";
+#endif
+
+    string[] groups = 
+        File.ReadAllText(fileName)
+        .ReplaceLineEndings("\n")
+        .Split("\n\n")
+        .ToArray();
+
+    Console.WriteLine("Part 1: " + Part1(groups));
+    Console.WriteLine("Part 2: " + Part2(groups));
+    Console.ReadLine();
+}
+
+long Part1(string[] groups)
+{
+    return groups
+        .Select(g => g.Replace("\n", ""))
+        .Select(g => g.Distinct())
+        .Select(g => g.Count())
+        .Sum();
+}
+
+long Part2(string[] groups)
+{
+    return groups
+        .Select(g => g.Split("\n").Select(s => s.AsEnumerable()))
+        .Select(g => g.Aggregate((a, b) => a.Intersect(b)))
+        .Select(g => g.Count())
+        .Sum();
+}
+

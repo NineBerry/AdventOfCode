0a53860f834d727a445e0402e62ea824669dd2ae
NineBerry <nineberry@nineberry.de>
2024-02-06 17:39:35 +0100

    2020 Day 15

---
+++ 2020/Day15/Program.cs
@@ -0,0 +1,49 @@
+﻿// #define Sample
+
+{
+
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2020\Day15\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2020\Day15\Full.txt";
+#endif
+
+    int[] startValues = File.ReadAllText(fileName)
+        .Split(",")
+        .Select(int.Parse)
+        .ToArray();
+
+    Console.WriteLine("Part 1: " + Solve(startValues, 2020));
+    Console.WriteLine("Part 2: " + Solve(startValues, 30_000_000)); 
+    Console.ReadLine();
+}
+
+long Solve(int[] start, int targetStep)
+{
+    Dictionary<int, int> spoken = [];
+    int turn = 1;
+    int next = 0;
+
+    foreach (int initial in start)
+    {
+        next = speak(initial, turn++);
+    }
+
+    while (turn < targetStep)
+    {
+        next = speak(next, turn++);
+    }
+    return next;
+
+
+    int speak(int number, int turn)
+    {
+        int nextNumber = spoken.TryGetValue(number, out int lastSpoken) ? turn - lastSpoken : 0;
+        spoken[number] = turn;
+        return nextNumber;
+    }
+
+
+
+}
+

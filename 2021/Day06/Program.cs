eba0dd5981c63a05d77e22a2bc13f352c8f956eb
NineBerry <nineberry@nineberry.de>
2024-02-12 17:33:16 +0100

    2021 Day 06

---
+++ 2021/Day06/Program.cs
@@ -0,0 +1,44 @@
+﻿// #define Sample
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2021\Day06\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2021\Day06\Full.txt";
+#endif
+
+    int[] fish = File.ReadAllText(fileName).Split(',').Select(s => int.Parse(s)).ToArray();
+
+    Console.WriteLine("Part 1: " + Solve(fish, 80));
+    Console.WriteLine("Part 2: " + Solve(fish, 256));
+    Console.ReadLine();
+}
+
+long Solve(int[] initialFish, int steps)
+{
+    long[] fish = new long[9];
+    for(int i = 0; i <= 8; i++)
+    {
+        fish[i] = initialFish.Count(f => f == i);
+    }
+
+    foreach(int step in Enumerable.Range(1, steps))
+    {
+        Step(fish);
+    }
+
+    return fish.Sum();
+}
+
+void Step(long[] fish)
+{
+    long zeros = fish[0];
+
+    for(int i = 0; i < 8; i++)
+    {
+        fish[i] = fish[i + 1];
+    }
+
+    fish[6] += zeros;
+    fish[8] = zeros;
+}

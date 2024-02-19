09fea6c82611d61d873691d5f5216dd7591ff67e
NineBerry <nineberry@nineberry.de>
2024-02-19 17:21:36 +0100

    2022 Days 04 and 05

---
+++ 2022/Day04/Program.cs
@@ -0,0 +1,58 @@
+﻿// #define Sample
+
+using System.Text.RegularExpressions;
+using RangeCouple = (InclusiveRange First, InclusiveRange Second);
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day04\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day04\Full.txt";
+#endif
+
+    RangeCouple[] couples = File.ReadAllLines(fileName).Select(Parse).ToArray();
+
+    Console.WriteLine("Part 1: " + Part1(couples));
+    Console.WriteLine("Part 2: " + Part2(couples));
+    Console.ReadLine();
+}
+
+long Part1(RangeCouple[] couples)
+{
+    return couples.Count(c => c.First.FullyIncludes(c.Second) || c.Second.FullyIncludes(c.First));
+}
+
+long Part2(RangeCouple[] couples)
+{
+    return couples.Count(c => c.First.Overlaps(c.Second));
+}
+
+
+RangeCouple Parse(string line)
+{
+    int[] values = Regex.Matches(line, "\\d+").Select(m => int.Parse(m.Value)).ToArray();
+    return (new InclusiveRange(values[0], values[1]), new InclusiveRange(values[2], values[3]));
+}
+
+
+public class InclusiveRange
+{
+    public InclusiveRange(int min, int max)
+    {
+        Min = min; 
+        Max = max;
+    }
+
+    public int Min;
+    public int Max;
+
+    public bool FullyIncludes(InclusiveRange other)
+    {
+        return (Min <= other.Min) && (Max >= other.Max);
+    }
+    public bool Overlaps(InclusiveRange other)
+    {
+        return FullyIncludes(other) || (Min >= other.Min && Min <= other.Max) || (Max >= other.Min && Max <= other.Max);
+    }
+}
+
---
+++ 2022/Day05/Program.cs
@@ -0,0 +1,119 @@
+﻿// #define Sample
+
+using System.Text.RegularExpressions;
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day05\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day05\Full.txt";
+#endif
+
+    string[] lines = File.ReadAllLines(fileName);
+
+    Instruction[] instructions = lines.SkipWhile(s => s != "").Skip(1).Select(s => new Instruction(s)).ToArray();
+    string[] boxStacksDefinition = lines.TakeWhile(s => s != "").ToArray();
+
+    Console.WriteLine("Part 1: " + Part1(boxStacksDefinition, instructions));
+    Console.WriteLine("Part 2: " + Part2(boxStacksDefinition, instructions));
+    Console.ReadLine();
+}
+
+string Part1(string[] boxStacksDefinition, Instruction[] instructions)
+{
+    BoxStacks boxStacks = new(boxStacksDefinition);
+    
+    foreach (var instruction in instructions)
+    {
+        boxStacks.ApplyPart1(instruction);
+    }
+
+    return boxStacks.GetTopString();
+}
+
+string Part2(string[] boxStacksDefinition, Instruction[] instructions)
+{
+    BoxStacks boxStacks = new(boxStacksDefinition);
+
+    foreach (var instruction in instructions)
+    {
+        boxStacks.ApplyPart2(instruction);
+    }
+
+    return boxStacks.GetTopString();
+}
+
+
+public record Instruction
+{
+    public int Count;
+    public int From;
+    public int To;
+
+    public Instruction(string line)
+    {
+        int[] values = Regex.Matches(line, "\\d+").Select(m => int.Parse(m.Value)).ToArray();
+        Count = values[0];
+        From = values[1];
+        To = values[2];
+    }
+}
+
+public class BoxStacks
+{
+    public BoxStacks(string[] input)
+    {
+        stacks = Enumerable.Range(0, 10).Select(n => new Stack<char>()).ToArray();
+
+        var matches = Regex.Matches(input.Last(), "[0-9]").ToArray();
+
+        foreach (var match in matches)
+        {
+            int number = int.Parse(match.Value);
+            int stringPos = match.Index;
+            for(int line = input.Length - 2; line >= 0; line--)
+            {
+                char ch = input[line][stringPos];
+                if(ch != ' ') stacks[number].Push(ch);
+            }
+        }
+    }
+
+    public string GetTopString()
+    {
+        return string.Join("", stacks.Select(stack => stack.Count == 0 ? "" : "" + stack.Peek()));
+    }
+
+    public void ApplyPart1(Instruction instruction)
+    {
+        var from = stacks[instruction.From];
+        var to = stacks[instruction.To];
+
+        foreach (var _ in Enumerable.Range(1, instruction.Count)) 
+        { 
+            var ch = from.Pop();    
+            to.Push(ch);
+        }
+    }
+
+    public void ApplyPart2(Instruction instruction)
+    {
+        var from = stacks[instruction.From];
+        var to = stacks[instruction.To];
+
+        Stack<char> temp = [];
+
+        foreach (var _ in Enumerable.Range(1, instruction.Count))
+        {
+            var ch = from.Pop();
+            temp.Push(ch);
+        }
+
+        while(temp.TryPop(out char ch))
+        {
+            to.Push(ch);
+        }
+    }
+
+    private Stack<char>[] stacks = [];
+}

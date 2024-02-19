a5e2e65b910a9e90e2ad575ba4d7b0bffde9c531
NineBerry <nineberry@nineberry.de>
2024-02-19 17:39:52 +0100

    2022 Day 06

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,26 @@
+﻿// #define Sample
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day06\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2022\Day06\Full.txt";
+#endif
+
+    string input = File.ReadAllText(fileName);
+
+    Console.WriteLine("Part 1: " + FindMarker(input, 4));
+    Console.WriteLine("Part 2: " + FindMarker(input, 14));
+    Console.ReadLine();
+}
+
+long FindMarker(string input, int markerLength)
+{
+    for(int i=0; i < input.Length; i++)
+    {
+        string sub = input.Substring(i, markerLength);
+        if (sub.Distinct().Count() == markerLength) return i + markerLength;
+    }
+    
+    return 0;
+}

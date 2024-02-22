687b957beb651ebdab092704472fefd13d9c0cf7
NineBerry <nineberry@nineberry.de>
2024-02-22 21:10:18 +0100

    2019 Day 18 Part 2

--- 2019/Day18/Program.cs
+++ 2019/Day18/Program.cs
@@ -2,15 +2,19 @@
 
 {
 #if Sample
-    string fileName = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Sample.txt";
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Sample.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Sample2.txt";
 #else
-    string fileName = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Full.txt";
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Full.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2019\Day18\Full.txt";
 #endif
 
-    string[] input = File.ReadAllLines(fileName);
-    Grid grid = new Grid(input);
+    Grid gridPart1 = new Grid(File.ReadAllLines(fileNamePart1));
+    Console.WriteLine("Part 1: " + Part1(gridPart1));
 
-    Console.WriteLine("Part 1: " + Part1(grid));
+    Grid gridPart2 = new Grid(File.ReadAllLines(fileNamePart2));
+    Console.WriteLine("Part 2: " + Part2(gridPart2));
+
     Console.ReadLine();
 }
 
@@ -18,6 +22,13 @@ long Part1(Grid grid)
 {
     return grid.GetShortestPath();
 }
+
+long Part2(Grid grid)
+{
+    grid.BuildWalls();
+    return grid.GetShortestPathWithRobots();
+}
+
 
 public class Grid
 {
@@ -97,7 +108,10 @@ public class Grid
             {
                 if (CanEnter(neighbor, current.FoundKeys))
                 {
-                    todos.Enqueue((neighbor, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                    if (!visited.Contains((neighbor, current.FoundKeys)))
+                    {
+                        todos.Enqueue((neighbor, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                    }
                 }
             }
         }
@@ -125,6 +139,160 @@ public class Grid
     private static bool IsDoor(char ch)
     {
         return ch is >= 'A' and <= 'Z';
+    }
+
+    public void BuildWalls()
+    {
+        Walls.Add(Start);
+        foreach (var neighbor in Start.GetAllNeighboringPoints()) 
+        {
+            Walls.Add(neighbor);
+        }
+    }
+
+    public long GetShortestPathWithRobots()
+    {
+        int maxSteps = 0;
+        int maxKeys = 0;
+
+        Point robot1Start = Start with { X = Start.X - 1, Y = Start.Y - 1 };
+        Point robot2Start = Start with { X = Start.X + 1, Y = Start.Y - 1 };
+        Point robot3Start = Start with { X = Start.X - 1, Y = Start.Y + 1 };
+        Point robot4Start = Start with { X = Start.X + 1, Y = Start.Y + 1 };
+
+        HashSet<(Point Point, string FoundKeys)> visited = [];
+        PriorityQueue<(int ActiveRobot, Point Robot1, Point Robot2, Point Robot3, Point Robot4, int Steps, string FoundKeys), int> todos = new();
+        todos.Enqueue((1, robot1Start, robot2Start, robot3Start, robot4Start, 0, ""), 0);
+        todos.Enqueue((2, robot1Start, robot2Start, robot3Start, robot4Start, 0, ""), 0);
+        todos.Enqueue((3, robot1Start, robot2Start, robot3Start, robot4Start, 0, ""), 0);
+        todos.Enqueue((4, robot1Start, robot2Start, robot3Start, robot4Start, 0, ""), 0);
+
+        while (todos.TryDequeue(out var current, out _))
+        {
+            if (current.Steps > maxSteps + 49 || current.FoundKeys.Length > maxKeys)
+            {
+                maxSteps = current.Steps;
+                maxKeys = current.FoundKeys.Length;
+                // Console.Write("\r" + maxSteps + " " + maxKeys);
+            }
+
+            bool useRobot1 = true;
+            bool useRobot2 = true;
+            bool useRobot3 = true;
+            bool useRobot4 = true;
+
+            int keysBefore = current.FoundKeys.Length;
+
+            CheckKeyFound(ref current, current.Robot1);
+            CheckKeyFound(ref current, current.Robot2);
+            CheckKeyFound(ref current, current.Robot3);
+            CheckKeyFound(ref current, current.Robot4);
+
+            bool allowSwitching = keysBefore != current.FoundKeys.Length;
+
+
+            if (current.FoundKeys.Length == KeysCount)
+            {
+                return current.Steps;
+            }
+
+
+            if (visited.Contains((current.Robot1, current.FoundKeys)))
+            {
+                useRobot1 = false;
+            }
+
+            if (visited.Contains((current.Robot2, current.FoundKeys)))
+            {
+                useRobot2 = false;
+            }
+
+            if (visited.Contains((current.Robot3, current.FoundKeys)))
+            {
+                useRobot3 = false;
+            }
+
+            if (visited.Contains((current.Robot4, current.FoundKeys)))
+            {
+                useRobot4 = false;
+            }
+
+            visited.Add((current.Robot1, current.FoundKeys));
+            visited.Add((current.Robot2, current.FoundKeys));
+            visited.Add((current.Robot3, current.FoundKeys));
+            visited.Add((current.Robot4, current.FoundKeys));
+
+
+            if (useRobot1 && (current.ActiveRobot == 1 || allowSwitching))
+            {
+                foreach (var neighbor in current.Robot1.GetAllNeighboringPoints())
+                {
+                    if (CanEnter(neighbor, current.FoundKeys))
+                    {
+                        if (!visited.Contains((neighbor, current.FoundKeys)))
+                        {
+                            todos.Enqueue((1, neighbor, current.Robot2, current.Robot3, current.Robot4, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                        }
+                    }
+                }
+            }
+
+
+            if (useRobot2 && (current.ActiveRobot == 2 || allowSwitching))
+            {
+                foreach (var neighbor in current.Robot2.GetAllNeighboringPoints())
+                {
+                    if (CanEnter(neighbor, current.FoundKeys))
+                    {
+                        if (!visited.Contains((neighbor, current.FoundKeys)))
+                        {
+                            todos.Enqueue((2, current.Robot1, neighbor, current.Robot3, current.Robot4, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                        }
+                    }
+                }
+            }
+
+            if (useRobot3 && (current.ActiveRobot == 3 || allowSwitching))
+            {
+                foreach (var neighbor in current.Robot3.GetAllNeighboringPoints())
+                {
+                    if (CanEnter(neighbor, current.FoundKeys))
+                    {
+                        if (!visited.Contains((neighbor, current.FoundKeys)))
+                        {
+                            todos.Enqueue((3, current.Robot1, current.Robot2, neighbor, current.Robot4, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                        }
+                    }
+                }
+            }
+
+            if (useRobot4 && (current.ActiveRobot == 4 || allowSwitching))
+            {
+                foreach (var neighbor in current.Robot4.GetAllNeighboringPoints())
+                {
+                    if (CanEnter(neighbor, current.FoundKeys))
+                    {
+                        if (!visited.Contains((neighbor, current.FoundKeys)))
+                        {
+                            todos.Enqueue((4, current.Robot1, current.Robot2, current.Robot3, neighbor, current.Steps + 1, current.FoundKeys), current.Steps + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        return int.MaxValue;
+    }
+
+    private void CheckKeyFound(ref (int ActiveRobot, Point Robot1, Point Robot2, Point Robot3, Point Robot4, int Steps, string FoundKeys) current, Point checkKeyPoint)
+    {
+        if (KeyPositions.TryGetValue(checkKeyPoint, out var key))
+        {
+            if (!current.FoundKeys.Contains(key))
+            {
+                current.FoundKeys = new string((current.FoundKeys + key).Order().ToArray());
+            }
+        }
     }
 }
 
@@ -158,3 +326,5 @@ public enum Direction
     North,
     South,
 }
+
+

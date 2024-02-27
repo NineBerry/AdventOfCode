bf59bae341909fb71342adb215d0227218b869cf
NineBerry <nineberry@nineberry.de>
2024-02-27 21:48:58 +0100

    2019 Day 05 and Day 07

---
+++ 2019/Day07/Computer.cs
@@ -0,0 +1,286 @@
+﻿public class Computer
+{
+    public delegate int DoInput(Computer computer);
+    public delegate void DoOutput(Computer computer, int value);
+    public event DoInput? Input = null;
+    public event DoOutput? Output = null;
+
+    public int[] Memory = [];
+    public int Pointer;
+
+    public Computer(int[] program)
+    {
+        Memory = [.. program];
+    }
+
+    public void Run()
+    {
+        Pointer = 0;
+
+        while (Pointer >= 0)
+        {
+            var instruction = CreateInstruction();
+            Pointer += instruction.Execute(this);
+        }
+    }
+
+    public int SetValue(int paramNumber, int originalOpcodeWithParameters, int valueToSet)
+    {
+        int parameterValue = Memory[Pointer + paramNumber];
+        ParameterMode mode = GetParameterMode(paramNumber, originalOpcodeWithParameters);
+
+        return SetValue(mode, parameterValue, valueToSet);
+    }
+
+    public int SetValue(ParameterMode mode, int parameterValue, int valueToSet)
+    {
+        return mode switch
+        {
+            ParameterMode.PositionMode => Memory[parameterValue] = valueToSet,
+            ParameterMode.ImmediateMode => throw new ApplicationException("Cannot set value in immediate mode"),
+            _ => throw new ApplicationException("Invalide Parametermode")
+        };
+    }
+
+    public int GetValue(int paramNumber, int originalOpcodeWithParameters)
+    {
+        int parameterValue = Memory[Pointer + paramNumber];
+        ParameterMode mode = GetParameterMode(paramNumber, originalOpcodeWithParameters);
+
+        return GetValue(mode, parameterValue);
+    }
+
+    public int GetValue(ParameterMode mode, int value)
+    {
+        return mode switch
+        {
+            ParameterMode.PositionMode => Memory[value],
+            ParameterMode.ImmediateMode => value,
+            _ => throw new ApplicationException("Invalide Parametermode")
+        };
+    }
+
+    public int GetInput()
+    {
+        if (Input == null) throw new ApplicationException("Input event not set");
+        return Input(this);
+    }
+
+    public void SetOutput(int value)
+    {
+        if (Output == null) throw new ApplicationException("Output event not set");
+        Output(this, value);
+    }
+
+    private ParameterMode GetParameterMode(int paramNumber, int originalOpcodeWithParameters)
+    {
+        int modeBase = paramNumber switch
+        {
+            1 => originalOpcodeWithParameters / 100,
+            2 => originalOpcodeWithParameters / 1000,
+            3 => originalOpcodeWithParameters / 10000,
+            _ => throw new ApplicationException("Invalid paramNumber")
+        };
+
+        modeBase %= 10;
+
+        return (ParameterMode)modeBase;
+    }
+
+    private Instruction CreateInstruction()
+    {
+        int opCodeWithParameters = Memory[Pointer];
+        int opCode = opCodeWithParameters % 100;
+
+        return opCode switch
+        {
+            1 => new AddInstruction(opCodeWithParameters),
+            2 => new MultiplyInstruction(opCodeWithParameters),
+            3 => new InputInstruction(opCodeWithParameters),
+            4 => new OutputInstruction(opCodeWithParameters),
+            5 => new JumpIfTrueInstruction(opCodeWithParameters),
+            6 => new JumpIfFalseInstruction(opCodeWithParameters),
+            7 => new LessThanInstruction(opCodeWithParameters),
+            8 => new EqualsInstruction(opCodeWithParameters),
+            99 => new ExitInstruction(opCodeWithParameters),
+            _ => throw new ApplicationException("Unknown opcode")
+        };
+    }
+
+    public enum ParameterMode
+    {
+        PositionMode = 0,
+        ImmediateMode = 1
+    }
+}
+
+public abstract class Instruction
+{
+
+    public Instruction(int originalOpcode)
+    {
+        OriginalOpcodeWithParameters = originalOpcode;
+    }
+
+    public int OriginalOpcodeWithParameters;
+    public abstract int Execute(Computer computer);
+}
+
+
+public class ExitInstruction : Instruction
+{
+    public ExitInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        return -computer.Pointer - 2;
+    }
+}
+
+
+public class AddInstruction : Instruction
+{
+    public AddInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int operand1 = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int operand2 = computer.GetValue(2, OriginalOpcodeWithParameters);
+        int operationResult = operand1 + operand2;
+
+        computer.SetValue(3, OriginalOpcodeWithParameters, operationResult);
+
+        return 4;
+    }
+}
+
+public class MultiplyInstruction : Instruction
+{
+    public MultiplyInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int operand1 = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int operand2 = computer.GetValue(2, OriginalOpcodeWithParameters);
+        int operationResult = operand1 * operand2;
+
+        computer.SetValue(3, OriginalOpcodeWithParameters, operationResult);
+
+        return 4;
+    }
+}
+
+public class InputInstruction : Instruction
+{
+    public InputInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int value = computer.GetInput();
+        computer.SetValue(1, OriginalOpcodeWithParameters, value);
+        return 2;
+    }
+}
+
+public class OutputInstruction : Instruction
+{
+    public OutputInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int value = computer.GetValue(1, OriginalOpcodeWithParameters);
+        computer.SetOutput(value);
+        return 2;
+    }
+}
+
+public class JumpIfTrueInstruction : Instruction
+{
+    public JumpIfTrueInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int checkValue = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int gotoValue = computer.GetValue(2, OriginalOpcodeWithParameters);
+
+        if (checkValue != 0)
+        {
+            return gotoValue - computer.Pointer;
+        }
+        else
+        {
+            return 3;
+        }
+    }
+}
+
+public class JumpIfFalseInstruction : Instruction
+{
+    public JumpIfFalseInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int checkValue = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int gotoValue = computer.GetValue(2, OriginalOpcodeWithParameters);
+
+        if (checkValue == 0)
+        {
+            return gotoValue - computer.Pointer;
+        }
+        else
+        {
+            return 3;
+        }
+
+    }
+}
+
+public class LessThanInstruction : Instruction
+{
+    public LessThanInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int firstParameter = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int SecondParameter = computer.GetValue(2, OriginalOpcodeWithParameters);
+
+        int toStore = (firstParameter < SecondParameter) ? 1 : 0;
+        computer.SetValue(3, OriginalOpcodeWithParameters, toStore);
+
+        return 4;
+    }
+}
+
+public class EqualsInstruction : Instruction
+{
+    public EqualsInstruction(int originalOpcode) : base(originalOpcode)
+    {
+    }
+
+    public override int Execute(Computer computer)
+    {
+        int firstParameter = computer.GetValue(1, OriginalOpcodeWithParameters);
+        int SecondParameter = computer.GetValue(2, OriginalOpcodeWithParameters);
+
+        int toStore = (firstParameter == SecondParameter) ? 1 : 0;
+        computer.SetValue(3, OriginalOpcodeWithParameters, toStore);
+
+        return 4;
+    }
+}
---
+++ 2019/Day07/Program.cs
@@ -0,0 +1,159 @@
+﻿// #define Sample
+
+using System.Collections.Concurrent;
+
+{
+#if Sample
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2019\Day07\Sample.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2019\Day07\Sample2.txt";
+#else
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2019\Day07\Full.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2019\Day07\Full.txt";
+#endif
+
+    int[] programPart1 = ReadProgram(fileNamePart1);
+    Console.WriteLine("Part 1: " + Part1(programPart1));
+
+    int[] programPart2 = ReadProgram(fileNamePart2);
+    Console.WriteLine("Part 2: " + Part2(programPart2));
+    Console.ReadLine();
+}
+
+static int[] ReadProgram(string fileNamePart2)
+{
+    return File.ReadAllText(fileNamePart2).Split(',').Select(int.Parse).ToArray();
+}
+
+int Part1(int[] program)
+{
+    var combinations = Combinations.CreateCombinations(Enumerable.Range(0, 5).ToArray());
+    var values = combinations.Select(comb => TestCombination(program, comb)).ToArray();
+    return values.Max();
+}
+
+int TestCombination(int[] program, int[] combination)
+{
+    int output = 0;
+
+    foreach (int i in combination)
+    {
+        output = RunComputer(program, i, output);
+    }
+
+    return output;
+}
+
+int RunComputer(int[] program, int phaseSetting, int input)
+{
+    int output = 0;
+    bool firstCall = true;
+
+    Computer computer = new Computer(program);
+    computer.Input += (c) =>
+    {
+        if (firstCall)
+        {
+            firstCall = false;
+            return phaseSetting;
+        }
+        return input;
+    };
+    computer.Output += (c, value) => output = value;
+
+    computer.Run();
+    return output;
+}
+
+int Part2(int[] program)
+{
+    var combinations = Combinations.CreateCombinations(Enumerable.Range(5, 5).ToArray());
+    var values = combinations.Select(comb => TestCombinationWithFeedbackLoop(program, comb)).ToArray();
+    return values.Max();
+}
+
+int TestCombinationWithFeedbackLoop(int[] program, int[] combination)
+{
+    int output = 0;
+
+    Computer computerA = new Computer(program);
+    Computer computerB = new Computer(program);
+    Computer computerC = new Computer(program);
+    Computer computerD = new Computer(program);
+    Computer computerE = new Computer(program);
+
+    BlockingCollection<int> queueA = [];
+    BlockingCollection<int> queueB = [];
+    BlockingCollection<int> queueC = [];
+    BlockingCollection<int> queueD = [];
+    BlockingCollection<int> queueE = [];
+
+    computerA.Input += (c) => queueA.Take();
+    computerA.Output += (c, value) => queueB.Add(value);
+
+    computerB.Input += (c) => queueB.Take();
+    computerB.Output += (c, value) => queueC.Add(value);
+
+    computerC.Input += (c) => queueC.Take();
+    computerC.Output += (c, value) => queueD.Add(value);
+
+    computerD.Input += (c) => queueD.Take();
+    computerD.Output += (c, value) => queueE.Add(value);
+
+    computerE.Input += (c) => queueE.Take();
+    computerE.Output += (c, value) =>
+    {
+        output = value;
+        queueA.Add(value);
+    };
+
+    queueA.Add(combination[0]);
+    queueB.Add(combination[1]);
+    queueC.Add(combination[2]);
+    queueD.Add(combination[3]);
+    queueE.Add(combination[4]);
+
+    queueA.Add(0);
+
+    var taskA = Task.Factory.StartNew(() => computerA.Run(), TaskCreationOptions.LongRunning);
+    var taskB = Task.Factory.StartNew(() => computerB.Run(), TaskCreationOptions.LongRunning);
+    var taskC = Task.Factory.StartNew(() => computerC.Run(), TaskCreationOptions.LongRunning);
+    var taskD = Task.Factory.StartNew(() => computerD.Run(), TaskCreationOptions.LongRunning);
+    var taskE = Task.Factory.StartNew(() => computerE.Run(), TaskCreationOptions.LongRunning);
+
+    Task.WaitAll(taskA, taskB, taskC, taskD, taskE);
+
+    return output;
+}
+
+
+
+public static class Combinations
+{
+    public static IEnumerable<int[]> CreateCombinations(int[] numbers)
+    {
+        foreach (var combination in CreateCombinationsRecursive(numbers, []))
+        {
+            yield return combination;
+        }
+    }
+
+    private static IEnumerable<int[]> CreateCombinationsRecursive(int[] numbers, int[] soFar)
+    {
+        if (soFar.Length == numbers.Length)
+        {
+            yield return [.. soFar];
+        }
+        else
+        {
+            foreach (var i in numbers)
+            {
+                if (soFar.Contains(i)) continue;
+
+                foreach (var combination in CreateCombinationsRecursive(numbers, [.. soFar, i]))
+                {
+                    yield return combination;
+                }
+            }
+        }
+    }
+}

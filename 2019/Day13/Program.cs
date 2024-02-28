a26e686b36612da27ab306f27adc0646a8cc9b73
NineBerry <nineberry@nineberry.de>
2024-02-28 01:36:20 +0100

    2019 Day 13 Part 2

--- 2019/Day13/Program.cs
+++ 2019/Day13/Program.cs
@@ -3,29 +3,26 @@
 {
     string fileName = @"D:\Dropbox\Work\AdventOfCode\2019\Day13\Full.txt";
     string visualizationFileName = @"D:\Dropbox\Work\AdventOfCode\2019\Day13\Visualization.txt";
-    string visualizationFileName2 = @"D:\Dropbox\Work\AdventOfCode\2019\Day13\Visualization2.txt";
     long[] program = ReadProgram(fileName);
 
     Console.WriteLine("Part 1: " + Part1(program, visualizationFileName));
-    Console.WriteLine("Part 2: " + Part2(program, visualizationFileName2));
+    Console.WriteLine("Part 2: " + Part2(program, onScreen: false)); // Set true to see game live
 
     Console.ReadLine();
 }
 
-
 long Part1(long[] program, string visualizationFileName)
 {
-    (var blocksLeft, _) = Play(program, out var canvas);
+    (var blocksLeft, _) = Play(program, out var canvas, false);
     SavePainting(visualizationFileName, canvas);
     return blocksLeft;
 }
 
-long Part2(long[] program, string visualizationFileName)
+long Part2(long[] program, bool onScreen)
 {
     program = [2, .. program.Skip(1)];
 
-    (var blocksLeft, var playerScore) = Play(program, out var canvas);
-    SavePainting(visualizationFileName, canvas);
+    (var blocksLeft, var playerScore) = Play(program, out var canvas, onScreen);
 
     if(blocksLeft == 0)
     {
@@ -33,19 +30,15 @@ long Part2(long[] program, string visualizationFileName)
     }
 
     Console.WriteLine("Lost! Still " + blocksLeft + " blocks left");
-    return playerScore;
+    return 0;
 }
-
-// 28512 too high
-// 17280 too low
-// 21057 too low
 
 static long[] ReadProgram(string fileNamePart2)
 {
     return File.ReadAllText(fileNamePart2).Split(',').Select(long.Parse).ToArray();
 }
 
-(long BlocksLeft, long PlayerScore) Play(long[] program, out Dictionary<Point, Tile> outCanvas)
+(long BlocksLeft, long PlayerScore) Play(long[] program, out Dictionary<Point, Tile> outCanvas, bool onScreen)
 {
     Dictionary<Point, Tile> canvas = [];
 
@@ -54,23 +47,21 @@ static long[] ReadProgram(string fileNamePart2)
     int awaitingInput = 0;
     long x = 0;
     long y = 0;
+
     long playerScore = 0;
-    bool justTouched = false;   
-
+    Point ballPoint = new Point();
     Point paddlePoint = new Point();
-
-    long paddleTarget = 20;
+    
     computer.Input += (c) =>
     {
-        Console.Clear();
-        Console.WriteLine(ShowPainting(canvas, playerScore));
-        Thread.Sleep(100);
+        if (onScreen)
+        {
+            Console.Clear();
+            Console.WriteLine(ShowPainting(canvas, playerScore));
+            Thread.Sleep(1);
+        }
 
-        long paddlePosition = paddlePoint.X;
-
-        if(paddlePosition < paddleTarget) return 1;
-        if (paddlePosition > paddleTarget) return -1;
-        return 0;
+        return Math.Sign(ballPoint.X - paddlePoint.X);
     };
 
     computer.Output += (c, value) =>
@@ -89,36 +80,22 @@ static long[] ReadProgram(string fileNamePart2)
         {
             if (x == -1 && y == 0)
             {
-                if (value != 0)
-                {
-                    playerScore = value;
-                    Console.WriteLine("Score updated: " + value);
-                }
+                playerScore = value;
             }
             else
             {
-                if (justTouched)
-                {
-                    justTouched = false;
-
-                    if (paddleTarget == 30) paddleTarget = 24;
-                    if (paddleTarget == 36) paddleTarget = 30;
-                    if (paddleTarget == 34) paddleTarget = 36;
-                    if (paddleTarget == 27) paddleTarget = 34;
-                    if (paddleTarget == 20) paddleTarget = 27;
-                }
-
+                Tile tile = (Tile)value;
                 Point point = new Point(x, y);
-                canvas[point] = (Tile)value;
+                canvas[point] = tile;
 
-                if(value == (long)Tile.Paddle)
+                if (tile == Tile.Paddle)
                 {
                     paddlePoint = point;
                 }
 
-                if(value == (long)Tile.Ball && y == paddlePoint.Y - 1 && (x == paddlePoint.X || x == paddlePoint.X - 1 || x == paddlePoint.X + 1))
+                if (tile == Tile.Ball)
                 {
-                    justTouched = true;
+                    ballPoint = point;
                 }
             }
             awaitingInput = 0;
@@ -126,9 +103,7 @@ static long[] ReadProgram(string fileNamePart2)
     };
 
     computer.Run();
-
     outCanvas = canvas;
-
     return (canvas.Count(p => p.Value == Tile.Block), playerScore);
 }
 
@@ -155,7 +130,8 @@ string ShowPainting(Dictionary<Point, Tile> canvas, long playerScore)
                 Tile.Paddle => '─',
                 Tile.Wall => '█',
                 Tile.Block => '#',
-                Tile.Ball => 'O'
+                Tile.Ball => 'O',
+                _ => throw new NotImplementedException()
             };
             sb.Append(ch);
         }
@@ -172,20 +148,7 @@ void SavePainting(string fileName, Dictionary<Point, Tile> canvas)
     File.WriteAllText(fileName, ShowPainting(canvas, 0));
 }
 
-record struct Point(long X, long Y)
-{
-    public Point GetNeightboringPoint(Direction direction)
-    {
-        return direction switch
-        {
-            Direction.South => this with { Y = this.Y + 1 },
-            Direction.West => this with { X = this.X - 1 },
-            Direction.North => this with { Y = this.Y - 1 },
-            Direction.East => this with { X = this.X + 1 },
-            _ => throw new ArgumentException("Unknown direction", nameof(direction)),
-        };
-    }
-}
+record struct Point(long X, long Y);
 
 public enum Tile
 {
@@ -194,38 +157,4 @@ public enum Tile
     Block = 2,
     Paddle = 3,
     Ball = 4,
-}
-
-public enum Direction
-{
-    South,
-    West,
-    North,
-    East
-}
-
-static class Extensions
-{
-    public static Direction TurnRight(this Direction direction)
-    {
-        return direction switch
-        {
-            Direction.North => Direction.East,
-            Direction.East => Direction.South,
-            Direction.South => Direction.West,
-            Direction.West => Direction.North,
-            _ => throw new ArgumentException("Unknown direction", nameof(direction)),
-        };
-    }
-    public static Direction TurnLeft(this Direction direction)
-    {
-        return direction switch
-        {
-            Direction.North => Direction.West,
-            Direction.West => Direction.South,
-            Direction.South => Direction.East,
-            Direction.East => Direction.North,
-            _ => throw new ArgumentException("Unknown direction", nameof(direction)),
-        };
-    }
 }

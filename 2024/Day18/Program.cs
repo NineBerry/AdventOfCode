5f80e48bc30d65abdee18b5047cfd142c1058199
NineBerry <nineberry@nineberry.de>
2024-12-17 21:10:13 +0100

    2024 Day 18 Template

---
+++ 2024/Day18/Program.cs
@@ -0,0 +1,20 @@
+﻿#define Sample
+
+{
+#if Sample
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2024\Day18\Sample.txt";
+#else
+    string fileName = @"D:\Dropbox\Work\AdventOfCode\2024\Day18\Full.txt";
+#endif
+
+    File.ReadAllLines(fileName);
+
+    Console.WriteLine("Part 1: " + Part1());
+    // Console.WriteLine("Part 2: " + Part2());
+    Console.ReadLine();
+}
+
+long Part1()
+{
+    return 0;
+}

d186b669dcb43ef28caa13fb992967f6ff198028
NineBerry <nineberry@nineberry.de>
2024-12-17 10:35:03 +0100

    2024 Day 17

---
+++ 2024/Day17/Computer.cs
@@ -0,0 +1,204 @@
+﻿public class Computer
+{
+    public delegate long DoInput(Computer computer);
+    public delegate void DoOutput(Computer computer, long value);
+    public event DoInput? Input = null;
+    public event DoOutput? Output = null;
+
+    public long[] Program = [];
+    public long Pointer;
+
+    public long RegisterA = 0;
+    public long RegisterB = 0;
+    public long RegisterC = 0;
+
+    public Computer(long[] program)
+    {
+        Program = [..program];
+    }
+
+    public void Run()
+    {
+        Pointer = 0;
+
+        while (Pointer >= 0 && Pointer < Program.Length)
+        {
+            var instruction = CreateInstruction();
+            Pointer += instruction.Execute(this);
+        }
+    }
+
+    public long GetInput()
+    {
+        if (Input == null) throw new ApplicationException("Input event not set");
+        return Input(this);
+    }
+
+    public void SetOutput(long value)
+    {
+        if (Output == null) throw new ApplicationException("Output event not set");
+        Output(this, value);
+    }
+
+    public long GetCombo(long value)
+    {
+        return value switch
+        {
+            >= 0 and <= 3 => value,
+            4 => RegisterA,
+            5 => RegisterB,
+            6 => RegisterC,
+            _ => throw new Exception("Invalid Combo " + value)
+        };
+    }
+
+    private long GetMemory(long index)
+    {
+        return Program[index];
+    }
+
+    private Instruction CreateInstruction()
+    {
+        long opcode = GetMemory(Pointer);
+        long operand = GetMemory(Pointer + 1);
+
+        return opcode switch
+        {
+            0 => new ADVInstruction(operand),
+            1 => new BXLInstruction(operand),
+            2 => new BSTInstruction(operand),
+            3 => new JNZInstruction(operand),
+            4 => new BXCInstruction(operand),
+            5 => new OUTInstruction(operand),
+            6 => new BDVInstruction(operand),
+            7 => new CDVInstruction(operand),
+            _ => throw new ApplicationException("Unknown opcode")
+        };
+    }
+}
+
+public abstract class Instruction
+{
+
+    public Instruction(long operand)
+    {
+        Operand = operand;
+    }
+
+    public long Operand;
+    public abstract long Execute(Computer computer);
+}
+
+public class ADVInstruction : Instruction
+{
+    public ADVInstruction(long operand) : base(operand)
+    {
+    }
+
+    protected long DoCalc(Computer computer)
+    {
+        int exponent = (int)computer.GetCombo(Operand);
+        if (exponent == 0) return computer.RegisterA;
+        if (exponent < 0) throw new Exception("Negative exponent");
+        return computer.RegisterA / (2L << (exponent - 1));
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterA = DoCalc(computer);
+        return 2;
+    }
+}
+
+public class BXLInstruction : Instruction
+{
+    public BXLInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterB = computer.RegisterB ^ Operand;
+        return 2;
+    }
+}
+
+public class BSTInstruction : Instruction
+{
+    public BSTInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterB = computer.GetCombo(Operand) % 8;
+        return 2;
+    }
+}
+
+public class JNZInstruction : Instruction
+{
+    public JNZInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        if (computer.RegisterA == 0) return 2;
+        return -computer.Pointer + Operand;
+    }
+}
+
+
+public class BXCInstruction : Instruction
+{
+    public BXCInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterB = computer.RegisterB ^ computer.RegisterC;
+        return 2;
+    }
+}
+
+public class OUTInstruction : Instruction
+{
+    public OUTInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        long value = computer.GetCombo(Operand) % 8;
+        computer.SetOutput(value);
+        return 2;
+    }
+}
+
+public class BDVInstruction : ADVInstruction
+{
+    public BDVInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterB = DoCalc(computer);
+        return 2;
+    }
+}
+
+public class CDVInstruction : ADVInstruction
+{
+    public CDVInstruction(long operand) : base(operand)
+    {
+    }
+
+    public override long Execute(Computer computer)
+    {
+        computer.RegisterC = DoCalc(computer);
+        return 2;
+    }
+}
---
+++ 2024/Day17/Program.cs
@@ -0,0 +1,128 @@
+﻿// #define Sample
+
+using System.Text.RegularExpressions;
+
+{
+#if Sample
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2024\Day17\Sample.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2024\Day17\Sample2.txt";
+#else
+    string fileNamePart1 = @"D:\Dropbox\Work\AdventOfCode\2024\Day17\Full.txt";
+    string fileNamePart2 = @"D:\Dropbox\Work\AdventOfCode\2024\Day17\Full.txt";
+#endif
+
+    Console.WriteLine("Part 1: " + Part1(fileNamePart1));
+    Console.WriteLine("Part 2: " + Part2(fileNamePart2));
+    Console.ReadLine();
+}
+
+string Part1(string fileName)
+{
+    var parsed = ParseFile(fileName); 
+    var output = RunComputer(parsed.Program, parsed.RegisterA);
+    return string.Join(',', output);
+}
+
+long Part2(string fileName)
+{
+    long[] program = ParseFile(fileName).Program;
+
+    var cycles = FindCycles(program);
+    return LockPick(program, program.Length - 1, cycles, cycles.Last());
+}
+
+long[] FindCycles(long[] program)
+{
+    long test = 0;
+    long currentIncrement = 1;
+    long currentCycleCounter = 0;
+    int previousLength = 0;
+    List<long> cycles = [];
+
+    while (true)
+    {
+        var output = RunComputer(program, test);
+        currentCycleCounter++;
+
+        if (output.Length > previousLength)
+        {
+            // Console.WriteLine($"Found cycle {currentCycleCounter} at {test}");
+            
+            cycles.Add(currentIncrement);
+            previousLength = output.Length;
+            currentIncrement *= currentCycleCounter;
+            currentCycleCounter = 0;
+        }
+
+        if (output.Length == program.Length) break;
+
+        test += currentIncrement;
+    }
+
+    cycles.Add(test);
+    
+    return [.. cycles];
+}
+
+long LockPick(long[] program, int positionToMatch, long[] cycles, long test)
+{
+    long currentIncrement = cycles[positionToMatch];
+
+    foreach (var _ in Enumerable.Range(0, 8))
+    {
+        var output = RunComputer(program, test);
+        
+        if (positionToMatch == 0)
+        {
+            if (output.Zip(program).All(p => p.First == p.Second)) return test;
+        }
+        else
+        {
+            if (program[positionToMatch] == output[positionToMatch])
+            {
+                long result = LockPick(program, positionToMatch - 1, cycles, test);
+                if(result != 0) return result;  
+            }
+        }
+
+        test += currentIncrement;                    
+    }
+
+    return 0;
+}
+
+long[] RunComputer(long[] program, long registerA)
+{
+    Computer computer = new Computer(program);
+    computer.RegisterA = registerA;
+    List<long> output = [];
+
+    computer.Output += (Computer computer, long value) =>
+    {
+        output.Add(value);
+    };
+
+    computer.Run();
+
+    return output.ToArray();
+}
+
+(long[] Program, long RegisterA) ParseFile(string fileName)
+{
+    string inputText = File.ReadAllText(fileName);
+    long[] inputValues = Tools.ParseIntegers(inputText);
+    long[] program = inputValues.Skip(3).ToArray();
+
+    return (program, inputValues[0]);
+}
+
+public static class Tools
+{
+    public static long[] ParseIntegers(string input)
+    {
+        return
+            Regex.Matches(input, @"-?\d+")
+            .Select(m => long.Parse(m.Value))
+            .ToArray();
+    }
+}
